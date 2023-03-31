86adfcb927952fc5589332ea732880d0ac7b4370
LAKSAN7 <laksanandpowerl@gmail.com>
2023-03-31 14:39:47 +0530

    first commit

---
+++ day2/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Header from './Components/Header';
+import ProductList from './Components/Productlist';
+import Footer from './Components/Footer';
+
+function App() {
+  const products = [
+    {
+      id: 1,
+      name: 'Product 1',
+      image: '/path/https://tse2.mm.bing.net/th?id=OIP.s9SFi5JBz9msehIzxoexzAHaHa&pid=Api&P=0/product-1.jpg',
+      // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      image: '/path/to/product-2.jpg',
+      // description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      image: '/path/to/product-3.jpg',
+      // description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  ];
+
+  return (
+    <div>
+      <Header />
+      <ProductList products={products} />
+      <Footer />
+    </div>
+  );
+}
+
+
+export default App;
+
